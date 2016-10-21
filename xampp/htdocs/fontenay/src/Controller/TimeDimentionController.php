23ffcf7a2ecc4a961129e0ba3a4f9a7c3d61e84c
Sven Godo <svengodo@gmail.com>
2016-10-22 01:54:55 +0200

    Modify TimeDimention, add findPersonBirthdays()

--- xampp/htdocs/fontenay/src/Controller/TimeDimentionController.php
+++ xampp/htdocs/fontenay/src/Controller/TimeDimentionController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\I18n\Date;
+use Cake\Collection\Collection;
 
 /**
  * TimeDimention Controller
@@ -40,6 +42,76 @@ class TimeDimentionController extends AppController
         $this->set('timeDimention', $timeDimention);
         $this->set('_serialize', ['timeDimention']);
     }
+	
+	/**
+	 * Get person birthdays
+	 * 
+	 * @param string|null $id of birthdate
+	 * @return array
+	 */
+	 public function findPersonBirthdays($bDateId = null)
+	 {
+	 	
+		$bdate = $this->TimeDimention->get($bDateId);
+		
+		$birthdays = $this->TimeDimention->find('all', array(
+			'conditions' => array(
+				'TimeDimention.d' => $bdate['d'],
+				'TimeDimention.m' => $bdate['m'],
+				'TimeDimention.y >=' => $bdate['y']
+				),
+			'order'	=> ['TimeDimention.id' => 'ASC'],
+			'limit' => 108
+		));
+		$birthdays->select(['TimeDimention.id', 'TimeDimention.date']);
+		
+		//debug($birthdays->toArray());
+		//debug($birthdays[0]);
+		
+		//Filter $birthdays
+		$eventStart = new Date('2012-01-01');
+		$eventEnd = new Date('2014-01-01');
+		
+		//$r = new Collection(array('0'=>'0'));
+		$r = array();
+		foreach($birthdays as $bday) {
+			if (($bday->date >= $eventStart) and ($bday->date <= $eventEnd)) {
+				//$r->append($bday);
+				debug($bday->id);
+				$r[] = $bday;
+			}
+		}
+		
+		debug($r[0]->id);
+		 
+		 /*
+		$r = $birthdays->every(function ($bday) {
+			//return (($bday->date >= $eventStart) and ($bday->Date < $eventEnd));
+			
+			/*
+			debug($bday->date);
+			debug(new Date('2012-01-01'));
+			debug(($bday->date <= new Date('2012-01-01')));
+			debug($bday);
+			 * */
+			
+			//debug((($bday->date >= new Date('2012-01-01')) and ($bday->Date < new Date('2014-01-01'))));
+			//return (($bday->date >= new Date('2012-01-01')) and ($bday->Date < new Date('2015-01-01')));
+			//return (($bday->date >= new Date('2012-01-01')));
+			
+		//});
+		
+		/*  
+		$r = $birthdays->match(
+			[
+				'TimeDimention.date >=' => $eventStart,
+				'TimeDimention.date <=' => $eventEnd
+			]
+			
+		);
+		*/
+		debug($r);
+	 }
 
     /**
      * Add method
